f709247b0adebee5dc6a6b5fe5546fdd25e43357
michail82 <michail82@users.noreply.github.com>
2019-12-06 13:02:14 +0200

    update

--- step_14.py
+++ step_14.py
@@ -1,4 +1,33 @@
 a, b, c = int(input()), int(input()), int(input())
+a = (a + b + c) - (b + c)
+b = (a + b + c) - (a + c)
+c = (a + b + c) - (a + b)
+if b == c == a:
+    print(a, b, c, sep="\n")
+if a > b > c:
+    print(a, c, b, sep="\n")
+if a > c > b:
+    print(a, b, c, sep="\n")
+if a == c > b:
+    print(a, b, c, sep="\n")
+if a == c < b:
+    print(b, a, c, sep="\n")
+if a == b > c:
+    print(a, c, b, sep="\n")
+if a == b < c:
+    print(c, a, b, sep="\n")
+if b > a > c:
+    print(b, c, a, sep="\n")
+if b > c > a:
+    print(b, a, c, sep="\n")
+if b == c > a:
+    print(b, a, c, sep="\n")
+if b == c < a:
+    print(a, b, c, sep="\n")
+if c > a > b:
+    print(c, b, a, sep="\n")
+if c > b > a:
+    print(c, a, b, sep="\n")
 
 # Напишите программу, которая получает на вход три целых числа,
 # по одному числу в строке, и выводит на консоль в три строки сначала максимальное,
